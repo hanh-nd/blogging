f779683b5462725c60e3a8d853748e023e60c74c
hanhnd <hanhnd156@gmail.com>
2024-02-23 17:25:55 +0700

    feat: add user password data mapper implementation

--- user-service/src/dataaccess/db/mappers/user-password.dm.ts
+++ user-service/src/dataaccess/db/mappers/user-password.dm.ts
@@ -1,21 +1,50 @@
+import { HttpStatus } from '@src/constants';
+import { ErrorWithStatus } from '@src/utils/errors';
 import { token } from 'brandi';
+import { DataSource, EntityManager } from 'typeorm';
+import { Logger } from 'winston';
 import { UserPassword } from '../entities';
 
 export interface UserPasswordDataMapper {
     from(body: Partial<UserPassword>): UserPassword;
     createUserPassword(body: UserPassword): Promise<UserPassword>;
     getUserPasswordByUserId(userId: number): Promise<UserPassword | null>;
+    withTransaction<T>(callback: (manager: EntityManager) => Promise<T>): Promise<T>;
 }
 
 export class UserPasswordDataMapperImpl implements UserPasswordDataMapper {
+    constructor(
+        private readonly dataSource: DataSource,
+        private readonly logger: Logger,
+    ) {}
+
     from(body: Partial<UserPassword>): UserPassword {
-        throw new Error('Method not implemented.');
+        return this.dataSource.getRepository(UserPassword).create(body);
     }
-    createUserPassword(body: UserPassword): Promise<UserPassword> {
-        throw new Error('Method not implemented.');
+
+    async createUserPassword(body: UserPassword): Promise<UserPassword> {
+        try {
+            return this.withTransaction(async (manager) => {
+                await manager.getRepository(UserPassword).softDelete({ userId: body.userId });
+                return manager.getRepository(UserPassword).save(body);
+            });
+        } catch (error) {
+            this.logger.error(`Failed to create user password: ${(error as Error).message}`, body);
+            throw ErrorWithStatus.wrapWithStatus(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
     getUserPasswordByUserId(userId: number): Promise<UserPassword | null> {
-        throw new Error('Method not implemented.');
+        try {
+            return this.dataSource.getRepository(UserPassword).findOneBy({ userId });
+        } catch (error) {
+            this.logger.error(`Failed to get user password by id: ${(error as Error).message}`, userId);
+            throw ErrorWithStatus.wrapWithStatus(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    withTransaction<T>(callback: (manager: EntityManager) => Promise<T>): Promise<T> {
+        return this.dataSource.transaction(callback);
     }
 }
 
