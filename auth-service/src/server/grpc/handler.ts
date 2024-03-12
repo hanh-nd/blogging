c567259eca8834ac04b5aebce4c5a14835afabcb
hanhnd <hanhnd156@gmail.com>
2024-03-12 14:48:51 +0700

    refractor(handler): wrap handler errors

--- auth-service/src/server/grpc/handler.ts
+++ auth-service/src/server/grpc/handler.ts
@@ -54,222 +54,135 @@ export class AuthServiceHandlersFactory {
     ) {}
 
     public getHandlers(): AuthServiceHandlers {
-        return {
+        const handlers: AuthServiceHandlers = {
             CreateUser: async ({ request }, callback) => {
-                try {
-                    const createdUser = await this.userManagementOperator.createUser(request as CreateUserRequest);
-                    return callback(null, { user: createdUser });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const createdUser = await this.userManagementOperator.createUser(request as CreateUserRequest);
+                return callback(null, { user: createdUser });
             },
             UpdateUser: async ({ request }, callback) => {
-                try {
-                    const updatedUser = await this.userManagementOperator.updateUser(request as UpdateUserRequest);
-                    return callback(null, { user: updatedUser });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const updatedUser = await this.userManagementOperator.updateUser(request as UpdateUserRequest);
+                return callback(null, { user: updatedUser });
             },
             DeleteUser: async ({ request }, callback) => {
-                try {
-                    const deleteResult = await this.userManagementOperator.deleteUser(request as DeleteUserRequest);
-                    return callback(null, { deleted: deleteResult });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const deleteResult = await this.userManagementOperator.deleteUser(request as DeleteUserRequest);
+                return callback(null, { deleted: deleteResult });
             },
             GetUserById: async ({ request }, callback) => {
-                try {
-                    const user = await this.userManagementOperator.getUserById(request as GetUserByIdRequest);
-                    return callback(null, { user: user });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const user = await this.userManagementOperator.getUserById(request as GetUserByIdRequest);
+                return callback(null, { user: user });
             },
             GetUserByUserName: async ({ request }, callback) => {
-                try {
-                    const user = await this.userManagementOperator.getUserByUserName(
-                        request as GetUserByUserNameRequest,
-                    );
-                    return callback(null, { user: user });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const user = await this.userManagementOperator.getUserByUserName(request as GetUserByUserNameRequest);
+                return callback(null, { user: user });
             },
             GetListUser: async ({ request }, callback) => {
-                try {
-                    const users = await this.userManagementOperator.getListUser(request as GetListUserRequest);
-                    return callback(null, { users: users });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const users = await this.userManagementOperator.getListUser(request as GetListUserRequest);
+                return callback(null, { users: users });
             },
             GetUserCount: async ({ request }, callback) => {
-                try {
-                    const count = await this.userManagementOperator.getUserCount(request as GetUserCountRequest);
-                    return callback(null, { count: count });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const count = await this.userManagementOperator.getUserCount(request as GetUserCountRequest);
+                return callback(null, { count: count });
             },
             CreateUserPassword: async ({ request }, callback) => {
-                try {
-                    const userPassword = await this.userPasswordManagementOperator.createUserPassword(
-                        request as CreateUserPasswordRequest,
-                    );
-                    return callback(null, { userPassword: userPassword });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const userPassword = await this.userPasswordManagementOperator.createUserPassword(
+                    request as CreateUserPasswordRequest,
+                );
+                return callback(null, { userPassword: userPassword });
             },
             GetUserPasswordByUserId: async ({ request }, callback) => {
-                try {
-                    const userPassword = await this.userPasswordManagementOperator.getUserPasswordByUserId(
-                        request as GetUserPasswordByUserIdRequest,
-                    );
-                    return callback(null, { userPassword: userPassword });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const userPassword = await this.userPasswordManagementOperator.getUserPasswordByUserId(
+                    request as GetUserPasswordByUserIdRequest,
+                );
+                return callback(null, { userPassword: userPassword });
             },
             LoginByPassword: async ({ request }, callback) => {
-                try {
-                    const authResponse = await this.userManagementOperator.loginByPassword(
-                        request as LoginByPasswordRequest,
-                    );
-                    return callback(null, authResponse);
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const authResponse = await this.userManagementOperator.loginByPassword(
+                    request as LoginByPasswordRequest,
+                );
+                return callback(null, authResponse);
             },
             GetUserRoles: async ({ request }, callback) => {
-                try {
-                    const roles = await this.userManagementOperator.getUserRoles(request as GetUserRolesRequest);
-                    return callback(null, { roles: roles });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const roles = await this.userManagementOperator.getUserRoles(request as GetUserRolesRequest);
+                return callback(null, { roles: roles });
             },
             CreateRole: async ({ request }, callback) => {
-                try {
-                    const role = await this.roleManagementOperator.createRole(request as CreateRoleRequest);
-                    return callback(null, { role: role });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const role = await this.roleManagementOperator.createRole(request as CreateRoleRequest);
+                return callback(null, { role: role });
             },
             UpdateRole: async ({ request }, callback) => {
-                try {
-                    const role = await this.roleManagementOperator.updateRole(request as UpdateRoleRequest);
-                    return callback(null, { role: role });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const role = await this.roleManagementOperator.updateRole(request as UpdateRoleRequest);
+                return callback(null, { role: role });
             },
             DeleteRole: async ({ request }, callback) => {
-                try {
-                    const deleted = await this.roleManagementOperator.deleteRole(request as DeleteRoleRequest);
-                    return callback(null, { deleted: deleted });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const deleted = await this.roleManagementOperator.deleteRole(request as DeleteRoleRequest);
+                return callback(null, { deleted: deleted });
             },
             GetRoleById: async ({ request }, callback) => {
-                try {
-                    const role = await this.roleManagementOperator.getRoleById(request as GetRoleByIdRequest);
-                    return callback(null, { role: role });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const role = await this.roleManagementOperator.getRoleById(request as GetRoleByIdRequest);
+                return callback(null, { role: role });
             },
             GetListRole: async ({ request }, callback) => {
-                try {
-                    const roles = await this.roleManagementOperator.getListRole(request as GetListRoleRequest);
-                    return callback(null, { roles: roles });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const roles = await this.roleManagementOperator.getListRole(request as GetListRoleRequest);
+                return callback(null, { roles: roles });
             },
             GetRoleCount: async ({ request }, callback) => {
-                try {
-                    const count = await this.roleManagementOperator.getRoleCount(request);
-                    return callback(null, { count: count });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const count = await this.roleManagementOperator.getRoleCount(request);
+                return callback(null, { count: count });
             },
             UpdateUserRoles: async ({ request }, callback) => {
-                try {
-                    const updated = await this.roleManagementOperator.updateUserRoles(
-                        request as UpdateUserRolesRequest,
-                    );
-                    return callback(null, { updated: updated });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const updated = await this.roleManagementOperator.updateUserRoles(request as UpdateUserRolesRequest);
+                return callback(null, { updated: updated });
             },
             CreatePermission: async ({ request }, callback) => {
-                try {
-                    const permission = await this.permissionManagementOperator.createPermission(
-                        request as CreatePermissionRequest,
-                    );
-                    return callback(null, { permission: permission });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const permission = await this.permissionManagementOperator.createPermission(
+                    request as CreatePermissionRequest,
+                );
+                return callback(null, { permission: permission });
             },
             UpdatePermission: async ({ request }, callback) => {
-                try {
-                    const permission = await this.permissionManagementOperator.updatePermission(
-                        request as UpdatePermissionRequest,
-                    );
-                    return callback(null, { permission: permission });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const permission = await this.permissionManagementOperator.updatePermission(
+                    request as UpdatePermissionRequest,
+                );
+                return callback(null, { permission: permission });
             },
             DeletePermission: async ({ request }, callback) => {
-                try {
-                    const deleted = await this.permissionManagementOperator.deletePermission(
-                        request as DeletePermissionRequest,
-                    );
-                    return callback(null, { deleted: deleted });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const deleted = await this.permissionManagementOperator.deletePermission(
+                    request as DeletePermissionRequest,
+                );
+                return callback(null, { deleted: deleted });
             },
             GetPermissionById: async ({ request }, callback) => {
-                try {
-                    const permission = await this.permissionManagementOperator.getPermissionById(
-                        request as GetPermissionByIdRequest,
-                    );
-                    return callback(null, { permission: permission });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const permission = await this.permissionManagementOperator.getPermissionById(
+                    request as GetPermissionByIdRequest,
+                );
+                return callback(null, { permission: permission });
             },
             GetListPermission: async ({ request }, callback) => {
-                try {
-                    const permissions = await this.permissionManagementOperator.getListPermission(
-                        request as GetListPermissionRequest,
-                    );
-                    return callback(null, { permissions: permissions });
-                } catch (error) {
-                    return this.handleError(error, callback);
-                }
+                const permissions = await this.permissionManagementOperator.getListPermission(
+                    request as GetListPermissionRequest,
+                );
+                return callback(null, { permissions: permissions });
             },
             UpdateRolePermissions: async ({ request }, callback) => {
+                const updated = await this.permissionManagementOperator.updateRolePermissions(
+                    request as UpdateRolePermissionsRequest,
+                );
+                return callback(null, { updated: updated });
+            },
+        };
+
+        Object.keys(handlers).forEach((key) => {
+            const originalHandler = handlers[key];
+            handlers[key] = async ({ request }: any, callback: any) => {
                 try {
-                    const updated = await this.permissionManagementOperator.updateRolePermissions(
-                        request as UpdateRolePermissionsRequest,
-                    );
-                    return callback(null, { updated: updated });
+                    return await originalHandler({ request } as any, callback);
                 } catch (error) {
                     return this.handleError(error, callback);
                 }
-            },
-        };
+            };
+        });
+
+        return handlers;
     }
 
     private handleError(error: unknown, callback: sendUnaryData<any>) {
